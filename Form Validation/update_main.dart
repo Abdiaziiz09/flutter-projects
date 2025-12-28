c5d8dfa5e333893e947172be81b54d643d194448
Abdiaziz Abdirahman <191661715+Abdiaziiz09@users.noreply.github.com>
2025-12-28 21:03:23 +0300

    Create update_main.dart

---
+++ Form Validation/update_main.dart
@@ -0,0 +1,206 @@
+// Created by: Abdiaziz Abdirahman Hassan
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}$'
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+  }
+}
+
+//  ========================
+//  Login page
+//  ========================
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff3F5FFF),
+        foregroundColor: Color(0xffffffff),
+        title: Text("Form Validation"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Form(
+              key: formKey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.black),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Fadlan geli email-kaaga!";
+                        }
+                        final regularExpression =
+                            r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}$';
+                        if (!RegExp(regularExpression).hasMatch(value)) {
+                          return "Fadlan geli email sax ah!";
+                        }
+                      },
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xffD0D0D0),
+                        prefixIcon: Icon(Icons.email),
+                        hintText: "Enter your email",
+                        hintStyle: TextStyle(color: Color(0xff4C4C4C)),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(
+                            width: 0.0,
+                            color: Colors.transparent,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(
+                            width: 0.0,
+                            color: Colors.transparent,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.black),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Fadlan geli password";
+                        }
+                        if (value.length < 7) {
+                          return "Password-kaaga haka bato ama hala ekaado 7";
+                        } else if (value.length > 10) {
+                          return "Password-kaaga haka yaraado ama hala ekaado 10";
+                        }
+                      },
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xffD0D0D0),
+                        prefixIcon: Icon(Icons.password),
+                        hintText: "Enter your password",
+                        hintStyle: TextStyle(color: Color(0xff4C4C4C)),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(
+                            width: 0.0,
+                            color: Colors.transparent,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(
+                            width: 0.0,
+                            color: Colors.transparent,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: GestureDetector(
+                              onTap: () {
+                                if (formKey.currentState!.validate()) {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) {
+                                        return HomePage();
+                                      },
+                                    ),
+                                  );
+                                }
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(12.0),
+                                decoration: BoxDecoration(
+                                  color: Color(0xff3F5FFF),
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Text(
+                                  "Login",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 17.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+        title: Text("Home page", style: GoogleFonts.acme(fontSize: 20.0)),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "Welcome to home page.",
+          style: GoogleFonts.acme(fontSize: 20.0),
+        ),
+      ),
+    );
+  }
+}
