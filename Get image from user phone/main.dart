47adb408e004992c57a661f1fa5c420fe4b155c4
Abdiaziz Abdirahman <ccasiiscraxmanxasan@gmail.com>
2026-01-15 17:49:36 +0300

    Create main.dart

---
+++ Get image from user phone/main.dart
@@ -0,0 +1,16 @@
+// Created by: Abdiaziz Abdirahman Hassan
+import 'package:flutter/material.dart';
+import 'package:test_app/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
